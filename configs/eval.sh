fc015bfe8bad9ae8bc87a771db55d5e7cf1ec7fa
王忠帅 <wangzhongshuaii@163.com>
2025-06-30 21:19:26 +0800

    Add files via upload

--- configs/eval.sh
+++ configs/eval.sh
@@ -13,3 +13,21 @@ python -u main.py \
     --resume ${RESUME} \
     --eval
     ${PY_ARGS}
+
+
+#!/usr/bin/env bash
+
+set -x
+BACKBONE='facebook/dinov2-base'
+EXP_DIR=./exps/xView2DOTA_uda/dinov2_projector_4_2_1_0.5_b4_base
+# PY_ARGS=${@:1}
+
+python -u main.py \
+    --output_dir ${EXP_DIR} \
+    --backbone ${BACKBONE} \
+    --batch_size 4 \
+    --projector_scale 4.0 2.0 1.0 0.5 \
+    # --resume ./exps/xView2DOTA/best_set/best_checkpoint.pth \
+    # --eval \
+    # --visualize \
+    # ${PY_ARGS}
